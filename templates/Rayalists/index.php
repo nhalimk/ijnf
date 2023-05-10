778f5ce6f2ae516318312efdedad25ca789f61c1
rlim <knorhalim@gmail.com>
2023-05-10 15:57:49 +0800

    update

--- templates/Rayalists/index.php
+++ templates/Rayalists/index.php
@@ -1,56 +1,68 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var iterable<\App\Model\Entity\Rayalist> $rayalists
- */
-?>
-<div class="rayalists index content">
-    <?= $this->Html->link(__('New Rayalist'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Rayalists') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('staffno') ?></th>
-                    <th><?= $this->Paginator->sort('fullname') ?></th>
-                    <th><?= $this->Paginator->sort('department') ?></th>
-                    <th><?= $this->Paginator->sort('tableno') ?></th>
-                    <th><?= $this->Paginator->sort('gate') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('updated') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($rayalists as $rayalist): ?>
-                <tr>
-                    <td><?= $this->Number->format($rayalist->id) ?></td>
-                    <td><?= h($rayalist->staffno) ?></td>
-                    <td><?= h($rayalist->fullname) ?></td>
-                    <td><?= h($rayalist->department) ?></td>
-                    <td><?= h($rayalist->tableno) ?></td>
-                    <td><?= h($rayalist->gate) ?></td>
-                    <td><?= h($rayalist->created) ?></td>
-                    <td><?= h($rayalist->updated) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $rayalist->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $rayalist->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $rayalist->id], ['confirm' => __('Are you sure you want to delete # {0}?', $rayalist->id)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+<div class="row">
+    <div class="card col-12">
+        <div class="card-header bg-primary text-white text-center">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h3><?= __('Attendance List') ?></h3>
+                </div>
+                <!--                <div class="col-md-6 text-right"><button class="btn btn-outline-success tect-right" id="print"><i class="fa fa-file-excel"></i> | Download Excel</button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>-->
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="attendances col-md-12">
+                <table class="table table-striped" cellpadding="0" cellspacing="0" id="table_staff">
+                    <thead>
+                        <tr>
+                            <th><?= $this->Paginator->sort('id') ?></th>
+                            <th><?= $this->Paginator->sort('staffno') ?></th>
+                            <th><?= $this->Paginator->sort('fullname') ?></th>
+                            <th><?= $this->Paginator->sort('department') ?></th>
+                            <th><?= $this->Paginator->sort('tableno') ?></th>
+                            <th><?= $this->Paginator->sort('gate') ?></th>
+                            <th><?= $this->Paginator->sort('created') ?></th>
+                            <th><?= $this->Paginator->sort('updated') ?></th>
+                            <th class="actions"><?= __('Actions') ?></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($rayalists as $rayalist) : ?>
+                            <tr>
+                                <td><?= $this->Number->format($rayalist->id) ?></td>
+                                <td><?= h($rayalist->staffno) ?></td>
+                                <td><?= h($rayalist->fullname) ?></td>
+                                <td><?= h($rayalist->department) ?></td>
+                                <td><?= h($rayalist->tableno) ?></td>
+                                <td><?= h($rayalist->gate) ?></td>
+                                <td><?= h($rayalist->created) ?></td>
+                                <td><?= h($rayalist->updated) ?></td>
+                                <td class="actions">
+                                    <?= $this->Html->link(__('View'), ['action' => 'view', $rayalist->id]) ?>
+                                    <?= $this->Html->link(__('Edit'), ['action' => 'edit', $rayalist->id]) ?>
+                                    <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $rayalist->id], ['confirm' => __('Are you sure you want to delete # {0}?', $rayalist->id)]) ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
+
+
+<?php $this->start('script'); ?>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#table_staff').DataTable({
+
+            "dom": 'Blftipr',
+            "buttons": [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5'
+            ],
+        });
+    });
+</script>
+<?php $this->end(); ?>
